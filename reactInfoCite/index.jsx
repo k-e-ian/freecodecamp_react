7e819a4ed01660556293689038cb5a384f8c3237
ian kitembe <ianedwin@outlook.com>
2023-09-26 12:49:51 +0300

    composibility introduction

--- reactInfoCite/index.jsx
+++ reactInfoCite/index.jsx
@@ -1,54 +1,83 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const navBar = (
-    <nav>
-        <h1>Ian's Website</h1>
-        <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+const reasonsToLearnReact = [
+    "It's a popular and in-demand skill in the job market.",
+    "It allows you to build interactive and dynamic web applications.",
+    "It has a strong and supportive community for learning and problem-solving.",
+    "It's backed by Facebook and used by many large companies.",
+    "It's component-based, making it easier to manage and reuse code.",
+  ];
+
+const ReasonsList = ({reasons}) => (
+    <ol>
+        {reasons.map((reason, index) => (
+            <li key={index}>
+                {reason}
+            </li>
+        ))}
+    </ol>
 )
 
-const page = (
-    <div>
-        <h1>My Awesome React Website</h1>
-        <h3>Reasons why I love React</h3>
-        <ol>
-            <li>It's composable</li>
-            <li>It's declarative</li>
-            <li>It's a hireable skill</li>
-            <li>It's actively maintained by skilled people</li>
-        </ol>
-    </div>
-)
+function Header() {
+    return (
+        <header>
+                <nav>
+                    <img class="react_img" src="./react.svg"></img>
+                </nav>
+        </header>
+    )
+}
 
-const funPage = (
-    <div>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>It's maintaned by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
 
-ReactDOM.render(
-    navBar,
-    document.getElementById("navBar")
-)
 
-ReactDOM.render(
-    page,
-    document.getElementById("root")
+function Page() {
+    return (
+        <>
+            <Header />
+            <div>
+                
+                <h1>Ian's Awesome React Website</h1>
+                <h2>Reasons React is cool</h2>
+                <ol>
+                    <li>It's composable</li>
+                    <li>It's declarative</li>
+                    <li>It's a hireable skill</li>
+                    <li>It's actively maintained by skilled people</li>
+                </ol>
+                <h2>Reasons I'm exited to write react code</h2>
+                <ReasonsList reasons={reasonsToLearnReact}/>
+            </div>
+            <div>
+                <h2>Fun facts about React</h2>
+                <ul>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100K stars on Github</li>
+                    <li>It's maintaned by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+                <small>&copy; 2023 Kitembe developement. All rights reserved</small>
+        </footer>
+    )
+}
+
+
+ReactDOM.render (
+    <Page/>,
+    document.getElementById("fun_page")
 )
 
 ReactDOM.render (
-    funPage,
-    document.getElementById("fun_page")
+    <Footer/>,
+    document.getElementById("footer")
 )
+
